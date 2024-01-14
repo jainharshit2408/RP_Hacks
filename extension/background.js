a067655d7253227555f193733775972c613c948e
Medhansh404 <21ucs126@lnmiit.ac.in>
2024-01-15 01:02:58 +0530

    revamp

--- extension/background.js
+++ extension/background.js
@@ -1,5 +1,11 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
-    alert(message);
-    // var response = "Hello to you too";
-    // sendResponse(response);
-});
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    alert("request.prediction");
+    if (request.prediction === true) {
+      alert("Warning: Phishing detected!! Phishing sites pretend to be other websites to trick you.");
+    } else if (request.prediction === -1) {
+      alert("No phishing detected");
+    }
+    sendResponse({ message: "Response from background script" });
+  }
+);
